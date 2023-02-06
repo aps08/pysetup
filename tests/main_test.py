0e9f1bb3419d591d438ab0a8aaf06f5c6b798b1f
aps08 <65893968+aps08@users.noreply.github.com>
2023-02-06 23:05:35 +0530

    added code to import from upper directory

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,2 +1,12 @@
-# your test cases goes here
+import os
+import sys
+import unittest
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+# above code so that you can import any thing from the src directory
+
 # Test file names should me *_test.py
+# your test cases goes here
